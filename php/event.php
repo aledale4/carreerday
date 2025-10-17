bc3b6816e0abba2d037ecd98e9bf000f3a13fecb
SimeDev42 <simeone.gen08@gmail.com>
2025-10-17 18:00:41 +0200

    Add event page

---
+++ php/event.php
@@ -0,0 +1,43 @@
+<div class="home-container">
+<div class="navbar">
+    <div class="left-side">
+        <img src="../static/logo.svg" alt="" srcset="" class="logo">
+        <p>Portale Studenti</p>
+    </div>
+    <div class="right-side">
+        <p>Benvenuto, <span><?php echo $_SESSION["user"]["nomeStu"]; ?></span></p>
+        <a href="index.php?pag=settings"><div class="user-pic"></div></a>
+    </div>
+</div>
+
+<div class="event">
+    <div class="event-title">
+        <a href="index.php"><span class="material-symbols-outlined">arrow_back_ios_new</span></a>
+        <p>Eventi / Career Day Informatica</p>
+    </div>
+    <div class="event-info">
+        <p class="event-date">15 ottobre 2025 / 10:00 - 13:00</p>
+        <span class="material-symbols-outlined">location_on</span>
+        <p class="event-location">Auditorium Cianfarani</p>
+    </div>
+    <p class="event-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id. Praesent lorem orci, mattis non efficitur id, ultricies vel nibh. Sed volutpat lacus vitae gravida viverra. Fusce vel tempor elit.</p>
+    <div class="event-participants">
+        <p>Con la partecipazione di:</p>
+        <div class="participants">
+            <div class="participant">
+                <p>Azienda 1</p>
+                <img src="" alt="">
+            </div>
+            <div class="participant">
+                <p>Azienda 2</p>
+                <img src="" alt="">
+            </div>
+            <div class="participant">
+                <p>Azienda 2</p>
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
--- php/index.php
+++ php/index.php
@@ -97,9 +97,11 @@
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/login_register.css">
     <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href="../css/event.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_back_ios_new,location_on" />
     <title>Career Day</title>
 </head>
 <body>
@@ -107,6 +109,8 @@
     if(isset($_SESSION["user"])){
         if($_GET["pag"] == "settings"){
             include("settings.php");
+        }else if($_GET["pag"] == "event"){
+            include("event.php");
         }else include("home.php");
     }
     else if(isset($_GET["pag"])){
