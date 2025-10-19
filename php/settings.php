52264dde76c5841893437de4e5e4a97f59c372b2
SimeDev42 <simeone.gen08@gmail.com>
2025-10-19 14:17:42 +0200

    fix settings page

--- php/settings.php
+++ php/settings.php
@@ -1,31 +1,37 @@
 <div class="home-container">
-    <div class="navbar-set">
-        <div class="leftside-set">
-            <div class="logo-set"><img src="../static/logo.svg" alt="" srcset=""></div>
+    <div class="navbar">
+        <div class="left-side">
+            <img src="../static/logo.svg" alt="" srcset="" class="logo">
             <p>Portale Studenti</p>
         </div>
     </div>
-    <div class="paolo"><a href="index.php"><span class="material-symbols-outlined">arrow_back_ios_new</span></a><p>Il tuo profilo</p></div>
-    <div class="card">
-        <div class="img"><img src="" alt=""></div>
-        <form action="">
-            <div>
-                <p><input type="hidden" name="pag" value="modifiche"></p>
-                <p><input type="text" name="nome" id="" placeholder="Nome" required></p>
-                <p><input type="text" name="cognome" id="" placeholder="Cognome" required></p>
-                <p><input type="text" name="username" id="" placeholder="Username" required></p>
-                <p><input type="email" name="email" id="" placeholder="Email" required></p>
-                <p><input type="password" name="password" id="" placeholder="Password" required></p>
-                <p><input type="password" name="newpassword" id="" placeholder="Nuova Password" required></p>
-            </div>
-            <div>
-                <h1>Dati Aggiuntivi</h1>
-                <p><input type="tel" placeholder="Numero di Telefono"></p>
-                <p><input type="text" placeholder="Localita"></p>
-                <p><input type="text" placeholder="Biografia"></p>
-                <p><input type="file" name="CV" id=""></p>
-            </div>
-            <p><input type="submit" value="Registrati"></p>
-        </form>
+    <div class="title-bar">
+        <a href="index.php"><span class="material-symbols-outlined">arrow_back_ios_new</span></a>
+        <p>Il tuo profilo</p>
     </div>
+    <form action="">
+    <div class="card">
+            <div class="side">
+                <div class="img"><img src="" alt=""></div>
+                <div class="inputs">
+                    <input type="hidden" name="pag" value="modifiche">
+                    <p>Nome: <input type="text" name="nome" id="" placeholder="Nome" required></p>
+                    <p>Cognome:<input type="text" name="cognome" id="" placeholder="Cognome" required></p>
+                    <p>Username:<input type="text" name="username" id="" placeholder="Username" required></p>
+                    <p>Email:<input type="email" Passwo name="email" id="" placeholder="Email" required></p>
+                    <p>Password Esistente:<input type="password" name="password" id="" placeholder="Password" required></p>
+                    <p>Nuova Password<input type="password" name="newpassword" id="" placeholder="Nuova Password" required></p>
+                </div>
+            </div>
+            <div class="side additional">
+                <h1>Dati Aggiuntivi</h1>
+                <div class="inputs">
+                    <p>Numero di Telefono:<input type="tel" placeholder="Numero di Telefono"></p>
+                    <p>Località:<input type="text" placeholder="Localita"></p>
+                    <p>Biografia:<input type="text" placeholder="Biografia"></p>
+                    <p>Curriculum:<input type="file" name="CV" id=""></p>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
