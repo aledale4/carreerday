6e3f959a2f3847bd1b99174f33ee64562a1e94d5
Loris_67 <difelice.loris.24052007@luigidisavoia.it>
2025-10-24 09:55:34 +0000

    modifica classi css send mail e reset pwd

--- php/request_reset_pwd.php
+++ php/request_reset_pwd.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="send_mail.css">
-    <title>Send-mail</title>
-</head>
-<body>
-    <div class="container_send_mail">
-        <h1>
-        Send Mail   
-        </h1>
-        <div class="container_form_send_mail">
+<div class="container_send_mail">
+    <h1>
+    Send Mail   
+    </h1>
+    <div class="container_form_send_mail">
         <form action="index.php" method="post" class="form_send_mail">
             <input type="email" id="email" name="email" placeholder="Email" required>
             <input type="hidden" name="pag" value="request_reset_pwd">
             <input type="submit" value="Send Mail">
         </form>
-        </div>
     </div>
-</body>
-</html>
+</div>
--- php/reset_pwd.php
+++ php/reset_pwd.php
@@ -1,25 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="reset_password.css">
-    <title>Reset-password</title>
-</head>
-<body>
-    <div class="container_reset_pwd">
-        <h1>
-            Reset Password
-        </h1>
-        <div class="form_container_reset_pwd">
-            <form action="reset-password.php" method="post" class="form_reset_pwd">
-                <input type="password" id="password" name="password" placeholder="Password" required>
-                <input type="password" id="password-confirm" name="password-confirm" placeholder="Password confirm" required>
-                <input type="hidden" name="pag" value="reset_pwd">
-                <input type="submit" value="Reset Password">
-            </form>
-        </div>
-      
+<div class="container_reset_pwd">
+    <h1>
+    Reset Password
+    </h1>
+    <div class="form_container_reset_pwd">
+        <form action="reset-password.php" method="post" class="form_reset_pwd">
+            <input type="password" id="password" name="password" placeholder="Password" required>
+            <input type="password" id="password-confirm" name="password-confirm" placeholder="Password confirm" required>
+            <input type="hidden" name="pag" value="reset_pwd">
+            <input type="submit" value="Reset Password">
+        </form>
     </div>
-</body>
-</html>
+</div>
