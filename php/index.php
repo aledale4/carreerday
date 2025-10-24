16bca3052bcf69cc219d4136ce034931ccd808fc
Loris_67 <difelice.loris.24052007@luigidisavoia.it>
2025-10-24 08:49:01 +0000

    Link css aggiunti del send mail e del reset password

--- php/index.php
+++ php/index.php
@@ -380,6 +380,8 @@
     <link rel="stylesheet" href="../css/new_edit_event.css">
     <link rel="stylesheet" href="../css/settings.css">
     <link rel="stylesheet" href="../css/company-home.css">
+    <link rel="stylesheet" href="../css/send_mail.css">
+    <link rel="stylesheet" href="../css/reset_password.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
