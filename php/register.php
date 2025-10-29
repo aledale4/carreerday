826a0492810fc054b6d4ec999906dd7773a74610
SimeDev42 <simeone.gen08@gmail.com>
2025-10-29 14:05:50 +0100

    typo

--- php/register.php
+++ php/register.php
@@ -48,7 +48,7 @@
                     </div>
                 </div>
                 <p class="p_di_register">
-                <input type="checkbox" id="policy-privacy" name="policy-privacy" value="mio cachbox" required>
+                <input type="checkbox" id="policy-privacy" name="policy-privacy" value="" required>
                 <label  class="" for="policy-privacy">Leggi e accetta la politica della privacy <a href="politica_privacy.php" style="color: blue; text-decoration:underline">qui</a>.</label>
                 </p>
                 <input type="submit" value="Registrati">
