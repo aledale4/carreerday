d59b1dc7da4ab51793e2acffadfd46fa2260b9a9
morganello76 <98307564+morganello76@users.noreply.github.com>
2025-11-05 20:23:20 +0000

    link feedback

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,7 @@ if($_SESSION["user-type"]==1){;
 ?>
 <footer>
     <p>Progetto realizzato per finalità didattiche<br>
-    <a href="https://docs.google.com/forms/d/1zExeTRC26eC2tleQB9-12VRemKARluD2lqcw7r-K_pA/edit?ts=690070aa">Lascia un feedback alla pagina</a>
+    <a href="https://forms.gle/gnvmgcyke81DPkPM8">Lascia un feedback alla pagina</a>
     </p>
     <p><a href="/php/politica_privacy.php">Politica Privacy</a></p>
 </footer>
@@ -18,7 +18,7 @@ if($_SESSION["user-type"]==2){;
 ?>
 <footer>
     <p>Progetto realizzato per finalità didattiche<br>
-    <a href="https://docs.google.com/forms/d/1_OUVs4gNr0LLGbz-5jmAcGIjNmt8dojgjPXFgyII7oE/edit?ts=69007577">Lascia un feedback alla pagina</a>
+    <a href="https://forms.gle/4gKX5mzdCB1UCSDb9">Lascia un feedback alla pagina</a>
     </p>
     <p><a href="/php/politica_privacy.php">Politica Privacy</a></p>
 </footer>
@@ -33,7 +33,7 @@ if($_SESSION["user-type"]==3){;
 ?>
 <footer>
     <p>Progetto realizzato per finalità didattiche<br>
-    <a href="https://docs.google.com/forms/d/135RQF7pO31SupSPLtMC0w5g1cY74ash_Pv-Lt8r2H6c/edit?ts=6900707f">Lascia un feedback alla pagina</a>
+    <a href="https://forms.gle/eBn1jxkSumZq2zUaA">Lascia un feedback alla pagina</a>
     </p>
     <p><a href="/php/politica_privacy.php">Politica Privacy</a></p>
 </footer>
