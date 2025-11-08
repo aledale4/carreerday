988f1286756ee3faa827890ba284ff7e7acddb99
SimeDev42 <simeone.gen08@gmail.com>
2025-11-08 16:53:19 +0100

    Homepage e settings studente

--- php/homepage.php
+++ php/homepage.php
@@ -10,16 +10,32 @@
         <h1>Incontra il tuo domani, oggi.</h1>
     </section>
     <section id="sect2" class="homepage-section">
-        <h1>Cos’è Career Day?</h1>
-         <p>Career Day è un progetto realizzato dalla classe 5BI a.s. 2025/2026 dell’Istituto “Luigi di Savoia” di Chieti, nato per favorire l’incontro tra studenti ed aziende. <br><br>Attraverso una piattaforma web dedicata, il Career Day permette di presentare profili, offrire opportunità di colloquio e creare un ponte diretto tra scuola e mondo del lavoro.</p>
+        <h1>Cos'è Career Day?</h1>
+         <p>Career Day è un'iniziativa dell'Istituto di Istruzione Superiore “Luigi di Savoia” di Chieti, nata per favorire l'incontro tra studenti ed aziende del territorio.
+        <br><br>
+        Attraverso una piattaforma web dedicata, sviluppata dagli studenti della classe 5B dell'indirizzo "Informatica e Telecomunicazioni", gli studenti potranno presentarsi alle aziende registrate e proporsi per un colloquio conoscitivo, creando un ponte diretto tra scuola e mondo del lavoro.
+    </p>
     </section>
     <section id="sect3" class="homepage-section">
-        <h1>Il nostro team di lavoro</h1>
-        <p>Nel corso di questo progetto, la classe 5ª B Informatica ha lavorato in modo collaborativo, mettendo in pratica competenze tecniche, organizzative e comunicative per realizzare un prodotto completo e ben strutturato.  <br><br> Il lavoro è stato suddiviso in più gruppi, ognuno con ruoli e responsabilità specifiche, ma tutti accomunati da un unico obiettivo: portare a termine un progetto ben documentato, testato e funzionante.  <br><br> Per gestire in modo efficace le varie fasi del progetto, la classe si è suddivisa in diversi gruppi di lavoro: gruppo di sviluppo, gruppo di documentazione, gruppo database e gruppo beta tester.  <br><br> Questo progetto ci ha insegnato che il successo nasce dalla collaborazione e dall’unione delle nostre capacità. Con impegno, fiducia e spirito di squadra, siamo riusciti a raggiungere un risultato di cui possiamo essere orgogliosi.</p>
+        <h1>La nostra scuola</h1>
+        <p>
+        La realizzazione di questa iniziativa è stata possibile grazie al contributo della scuola e dei suoi docenti.
+        <br><br>
+        L'Istituto di Istruzione Superiore «Luigi di Savoia», fondato nel 1879, è oggi un punto di riferimento nazionale per la formazione tecnica industriale.
+        <br><br>
+        La scuola offre due principali indirizzi: il Settore Tecnologico e il Liceo Scientifico opzione Scienze Applicate.
+        <br><br>
+        L'offerta formativa dell'IIS “Savoia” si distingue per l'uso di metodologie innovative e laboratoriali, che rendono gli studenti protagonisti del proprio apprendimento.
+        </p>
     </section>
     <section id="sect4" class="homepage-section">
-        <h1>La nostra scuola</h1>
-        <p>La realizzazione di questo progetto è stata possibile grazie al contributo della scuola e dei docenti.  <br><br> L’Istituto di Istruzione Superiore «Luigi di Savoia», fondato nel 1879, è oggi un punto di riferimento nazionale per la formazione tecnica industriale. <br><br> La scuola offre due principali indirizzi: il Settore Tecnologico e il Liceo Scientifico opzione Scienze Applicate.  <br><br>L’offerta formativa dell’IIS “Savoia” si distingue per l’uso di metodologie innovative e laboratoriali, che rendono gli studenti protagonisti del proprio apprendimento.  <br><br>La scuola dispone di laboratori specializzati, aule attrezzate, una grande Aula Magna di 400 mq, 3 palestre, ed una sala docenti accogliente.  <br><br>L’Istituto ha anche un Servizio di Prevenzione e Protezione che si occupa di tutti gli aspetti legati alla sicurezza.</p>
+        <h1>Il nostro team di lavoro</h1>
+        <p>Nel corso di questo progetto, la classe 5ª B Informatica ha lavorato in modo collaborativo, mettendo in pratica competenze tecniche, organizzative e comunicative volte a realizzare un prodotto completo e ben strutturato.
+        <br><br>
+        Il lavoro è stato suddiviso in più gruppi (gruppo di sviluppo, gruppo di documentazione, gruppo database e gruppo beta tester), ognuno con ruoli e responsabilità specifiche, ma tutti accomunati da un unico obiettivo: portare a termine un progetto ben documentato, testato e funzionante.
+        <br><br>
+        Questo progetto ci ha insegnato che il successo nasce dalla collaborazione e dall'unione delle nostre capacità e competenze. Con impegno, fiducia e spirito di squadra, siamo riusciti a raggiungere un risultato di cui siamo orgogliosi.
+        </p>
     </section>
 
 
